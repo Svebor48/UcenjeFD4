1f46d28d87795d6e4bbd4b2dbf47f57d665973b2
Svebor48 <svebor.kovacic1@gmail.com>
2026-07-14 15:59:35 +0200

    početak js

---
+++ 03_JS/vjezba01/skripta.js
@@ -0,0 +1,26 @@
+
+//komentar jedna linija
+
+/*
+
+komentar više linija
+
+*/
+
+//alert('Hello world 1') ; // ; može ali i ne mora ići
+
+//confirm('Hello world 2, pitanje')
+
+document.write('Hello world 3')
+
+document.getElementById('ispis').innerText = 'Hello world 7'
+document.getElementById('ispis').style.color = 'red'
+
+
+document.getElementById('ge').innerHTML = `
+
+    <a href="#">
+    Ovo je generirana poveznica
+    </a>
+
+`
